bea5c34dc396e65330adb84e01e71a6541cf757b
Md. Sadidur Rahman <mdsadidurrahman74@gmail.com>
2026-04-27 07:10:50 +0300

    chore: add location usage descriptions to Info.plist and marker UserDefaults keys

--- MapboxVelocity/MapboxVelocity/Core/Constants.swift
+++ MapboxVelocity/MapboxVelocity/Core/Constants.swift
@@ -12,4 +12,11 @@ enum UserDefaultsKeys {
         static let selectedMapStyle = "selected_map_style"
         static let overlayPrefix    = "overlay_"
     }
+
+    enum Marker {
+        static let label     = "marker_label"
+        static let shape     = "marker_shape"
+        static let color     = "marker_color"
+        static let imageData = "marker_image_data"
+    }
 }
