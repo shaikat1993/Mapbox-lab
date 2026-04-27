6081bf98280ad84676cc75278d4e08cb481ec1a7
Md. Sadidur Rahman <mdsadidurrahman74@gmail.com>
2026-04-27 07:11:02 +0300

    refactor: move GeocodingResult to Core/Models to respect single responsibility

---
+++ MapboxVelocity/MapboxVelocity/Core/Models/GeocodingResult.swift
@@ -0,0 +1,13 @@
+//
+//  GeocodingResult.swift
+//  MapboxVelocity
+//
+//  Created by Shaikat on 4/26/26.
+//
+
+import CoreLocation
+
+struct GeocodingResult {
+    let coordinate: CLLocationCoordinate2D
+    let address: String
+}
